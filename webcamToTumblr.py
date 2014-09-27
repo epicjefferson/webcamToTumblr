7006e98544e0d45d556b4d899a6711bad7f35fa1
epicjefferson <epicjefferson@gmail.com>
2014-09-27 12:44:26 -0400

    first commit

---
+++ webcamToTumblr.py
@@ -0,0 +1,48 @@
+'''webcamToTumblr project
+Epic Jefferson 
+
+A Raspberry Pi project.
+
+Using Tumblr's instructions for registering an app at https://www.tumblr.com/docs/en/api/v2
+and to get your consumer key,consumer secret, token key, and token secret.
+
+and adafruit's motion sensing lesson https://learn.adafruit.com/adafruits-raspberry-pi-lesson-12-sensing-movement
+'''
+
+#!/usr/bin/env python
+import sys
+import os
+import pytumblr
+import time
+import RPi.GPIO as io
+from tumblr_keys import *		#import your keys, you should save them in a separate tumblr_keys.py
+								#consumer_key = 'consumer key'
+								#consumer_secret = 'consumer secret'
+								#token_key = 'oauth token key' 
+								#token_secret = 'oauth token secret'
+
+io.setmode(io.BCM)
+pir_pin = 18					#connect the PIR sensor to pin 18 on the Pi's GPIO
+io.setup(pir_pin, io.IN)         # activate input
+
+# Authenticate via OAuth
+client = pytumblr.TumblrRestClient(
+	consumer_key,
+	consumer_secret,
+	token_key,
+	token_secret
+)
+
+while True:
+    print(io.input(pir_pin))	#print to check sensor state
+    if io.input(pir_pin):
+		os.system('sudo fswebcam -r 640x480 -S 20 webcam.jpg')
+
+		#Creates a photo post using a local filepath, 
+		#make sure you point to the appropriate image location, 
+		#In this example, the webcam.jpg was saved to the current folder
+		#Ex. from yourBlogName.tumblr.com should just use yourBlogName
+		#here the default state is set to "queue", to publish use "published"
+		client.create_photo('yourBlogName', state="published", tags=["testing", "ok"], data="webcam.jpg")
+		time.sleep(5)
+    time.sleep(0.25)
